9772fd201569bea7fec3cc076b18d81383dba478
ssantosfer <santosilvafernanda123@gmail.com>
2025-09-22 10:12:36 -0300

    File ingestion improvements

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -1,9 +1,12 @@
 from utils.utils import download_and_extract
 
-def ingest_empresas(bronze_path="data/bronze/empresas"):
-    url = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/2025-09/Empresas2.zip"
-    download_and_extract(url, bronze_path)
-
-def ingest_socios(bronze_path="data/bronze/socios"):
-    url = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/2025-09/Socios2.zip"
+def ingest(table: str, bronze_path_base="data/bronze"):
+    url = f"https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/2025-09/{table}.zip"
+    
+    table_map = {
+        "Empresas2": "empresas",
+        "Socios2": "socios"
+    }
+    folder_name = table_map.get(table, table.lower())
+    bronze_path = f"{bronze_path_base}/{folder_name}"
     download_and_extract(url, bronze_path)
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 from dotenv import load_dotenv
-from ingestion.ingest import ingest_empresas, ingest_socios
+from ingestion.ingest import ingest
 from transformation.transform import process_empresas, process_socios
 from load.persist import build_gold_sql
 
 def main():
     load_dotenv()
     conn_str = os.getenv("POSTGRES_CONN")
-    
-    ingest_empresas()
-    ingest_socios()
+
+    tables = ["Empresas2", "Socios2"]
+
+    for table in tables:
+        ingest(table)
+
     process_empresas()
     process_socios()
     build_gold_sql(conn_str)
